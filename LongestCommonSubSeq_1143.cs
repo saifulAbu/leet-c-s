d92c5e2ca32b22795a202890c986ac9eef20f4bf
REDMOND\saifabu <saifabu@microsoft.com>
2023-02-07 20:08:35 -0800

    adding one moer

---
+++ LongestCommonSubSeq_1143.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp
+{
+    internal class LongestCommonSubSeq_1143
+    {
+        Dictionary<int, int> dp = new Dictionary<int, int>();
+        string t0;
+        string t1;
+        int R, C;
+
+        public int LongestCommonSubsequence(string text1, string text2)
+        {
+            t0= text1;
+            t1= text2;
+
+            R = text1.Length ;
+            C = text2.Length ;
+
+            return ResolveEventArgs(R - 1, C - 1);
+        }
+
+        private int ResolveEventArgs(int i, int j)
+        {
+            if (i < 0 || j < 0) {
+                return 0;
+            }
+
+            int key = i * C + j ;
+
+            if (!dp.ContainsKey(key))
+            {
+                if(t0)
+            }
+            else { 
+            
+            }
+            
+        }
+    }
+}
---
+++ MaxScore_1770.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp
+{
+    internal class MaxScore_1770
+    {
+        int[] nums, multipliers;
+        Dictionary<int, int> dp = new Dictionary<int, int>();
+        int N;
+
+        public int MaximumScore(int[] nums, int[] multipliers)
+        {
+            this.nums = nums;
+            this.multipliers = multipliers;
+            this.N = nums.Length;
+            return soln(0, nums.Length - 1);
+        }
+
+        public int soln(int i, int j) {
+            int m = N + i - j - 1;
+            if (m == multipliers.Length)
+            {
+                return 0;
+            }
+            else {
+                int key = i * N + j;
+                if (!dp.ContainsKey(key))
+                {
+                    dp[key] = Math.Max(
+                                nums[i] * multipliers[m] + soln(i + 1, j),
+                                nums[j] * multipliers[m] + soln(i, j - 1)
+                    );
+                } 
+                return dp[key];
+            }
+        }
+    }
+}
