7b589786ab51543c8bde09499948efe47ea680a5
REDMOND\saifabu <saifabu@microsoft.com>
2024-02-21 07:47:43 -0800

    rebirth of the kid!

---
+++ 1295_find_even_digits.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp
+{
+    internal class _1295_find_even_digits
+    {
+        public int FindNumbers(int[] nums)
+        {
+            int count = 0;
+            foreach (int num in nums) { 
+                if (NumDigits(num) % 2 == 0) { 
+                    count++;
+                }
+            }
+            return count;
+        }
+        private int NumDigits(int num)
+        {
+            return (int)Math.Floor(Math.Log10(num)) + 1;
+        }
+
+    }
+}
---
+++ 977_squares_of_sorted_array.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp
+{
+    internal class _977_squares_of_sorted_array
+    {
+        public int[] SortedSquares(int[] nums)
+        {
+            return null;
+        }
+    }
+}
