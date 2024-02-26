f8e330d84f583eb416aaa02403187f937b3156d8
REDMOND\saifabu <saifabu@microsoft.com>
2024-02-26 10:01:34 -0800

    this moment!

---
+++ 1089_dup_zero.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp
+{
+    internal class _1089_dup_zero
+    {
+        public void DuplicateZeros(int[] arr)
+        {
+            Queue<int> q = new Queue<int>();
+
+            int i = 0;
+            while (i < arr.Length)
+            {
+                q.Enqueue(arr[i]);
+                int cur = q.Dequeue();
+                arr[i++] = cur;
+                if (cur == 0 && i < arr.Length) {
+                    q.Enqueue(arr[i]);
+                    arr[i++] = cur;
+                }
+            }
+        }
+    }
+}
--- 977_squares_of_sorted_array.cs
+++ 977_squares_of_sorted_array.cs
@@ -10,7 +10,45 @@ namespace c_sharp
     {
         public int[] SortedSquares(int[] nums)
         {
-            return null;
+            //square the input array
+            for (int k = 0; k < nums.Length; k++) 
+            {
+                nums[k] = nums[k] * nums[k];
+            }
+
+            // find input for minimum
+            int minIndex = 0;
+            int min = int.MaxValue;
+            for (int k = 0; k < nums.Length; k++) {
+                if (nums[k] < min)
+                { 
+                    min = nums[k];
+                    minIndex = k;
+                }
+            }
+
+            // copy into result array
+            int[] res = new int[nums.Length];
+            res[0] = nums[minIndex];
+            int left = minIndex - 1, right = minIndex + 1;
+
+            int i = 1;
+            while (left >=0 &&  right < res.Length) 
+            {
+                res[i++] = (nums[left] <= nums[right] ? nums[left--] : nums[right++]);
+            }
+
+            while (left >= 0)
+            {
+                res[i++] = nums[left--];
+            }
+
+            while (right < res.Length)
+            {
+                res[i++] = nums[right++];
+            }
+
+            return res;
         }
     }
 }
