86304561ea3c143c5a242d5878ba8598112b7f6a
REDMOND\saifabu <saifabu@microsoft.com>
2024-02-28 08:01:54 -0800

    2/28

---
+++ 26_RemoveDupsSortedArr.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp
+{
+    internal class _26_RemoveDupsSortedArr
+    {
+        public int RemoveDuplicates(int[] arr)
+        {
+            int k = 1;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[k - 1] != arr[i]) {
+                    arr[k++] = arr[i];
+                }
+            }
+            return k;
+        }
+    }
+}
---
+++ 27_RemoveElements.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp
+{
+    internal class _27_RemoveElements
+    {
+        public int RemoveElement(int[] arr, int val)
+        {
+            int k = 0;
+            while (k < arr.Length && arr[k] != val) {
+                k++;
+            }
+
+            for (int i = k;  i < arr.Length; i++)
+            {
+                if (arr[i] != val) 
+                {
+                    arr[k++] = arr[i];
+                }
+            }
+
+            return k;
+        }
+    }
+}
